8ae2405828ffd8be8f13517f86e34d5685df318a
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-28 16:02:05 +0200

    make routing work

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule,
+            DashboardRoutingModule,
+            MatIconModule,
+            MatButtonModule],
 })
 export class DashboardModule {}
