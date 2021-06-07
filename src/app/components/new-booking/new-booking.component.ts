d5f466dc6ec3e56eb64bbd64c40697fa4398481c
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-06-07 20:31:51 +0200

    add lens icons for courts select

--- src/app/components/new-booking/new-booking.component.ts
+++ src/app/components/new-booking/new-booking.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector:    'app-new-booking',
+  selector: 'app-new-booking',
   templateUrl: './new-booking.component.html',
-  styleUrls:   ['./new-booking.component.scss']
+  styleUrls: ['./new-booking.component.scss'],
 })
 export class NewBookingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/new-booking/new-booking.module.ts
+++ src/app/components/new-booking/new-booking.module.ts
@@ -9,19 +9,23 @@ import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [NewBookingComponent],
-  imports: [
-    CommonModule,
-    NewBookingRoutingModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatSelectModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule,
+        NewBookingRoutingModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatButtonModule
+    ],
 })
 export class NewBookingModule {}
