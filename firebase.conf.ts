fa228d147f6f800290f1dc580753a96409012da3
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-07-26 10:07:41 +0200

    firebaseConfig

---
+++ firebase.conf.ts
@@ -0,0 +1,11 @@
+import { FirebaseOptions } from '@angular/fire';
+
+export const firebaseConfig: FirebaseOptions = {
+  // apiKey: 'AIzaSyDX93xn8oTD_DjKAydW9Ptmn8Vur2Rdl-0',
+  authDomain: 'court-390a2.firebaseapp.com',
+  databaseURL: 'https://court-390a2.firebaseio.com',
+  projectId: 'court-390a2',
+  storageBucket: 'court-390a2.appspot.com',
+  messagingSenderId: '349565396591',
+  // appId: '1:349565396591:web:a45e707b7f53f824410a2d',
+};
